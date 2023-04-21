225d2f954af22643cdd97b74a54ab50a6411a194
Rochi Albani <rocio.albani@hotmail.com>
2023-04-21 00:44:26 -0300

    cors change

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -8,12 +9,20 @@ require('./db.js');
 
 const server = express();
 
+
+
 server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+server.use(cors({
+  credentials: true,
+  origin: 'https://pi-dogs-theta-beige.vercel.app',
+}));
+
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://pi-dogs-theta-beige.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
