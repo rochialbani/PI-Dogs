e7979dea8d41ab1da1a7d68842d494ac2a252bbe
Rochi Albani <rocio.albani@hotmail.com>
2023-05-03 18:37:06 -0300

    cors change 4

--- api/src/app.js
+++ api/src/app.js
@@ -23,13 +23,13 @@ server.use(cors({
   origin: '*',
 }));
 
-server.use((req, res, next) => {
+/*server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'false');
   res.header('Access-Control-Allow-Headers', '*');//Origin, X-Requested-With, Content-Type, Accept
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-});
+});*/
 
 server.use('/', routes);
 
