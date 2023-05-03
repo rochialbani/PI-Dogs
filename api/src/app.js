42aecbd9863869a1c662b85415e2d23439aebbcc
Rochi Albani <rocio.albani@hotmail.com>
2023-05-03 18:24:16 -0300

    cors change 3

--- api/src/app.js
+++ api/src/app.js
@@ -19,13 +19,13 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use(cors({
-  credentials: true,
-  origin: 'https://pi-dogs-theta-beige.vercel.app',
+  credentials: false,
+  origin: '*',
 }));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-dogs-theta-beige.vercel.app'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'false');
   res.header('Access-Control-Allow-Headers', '*');//Origin, X-Requested-With, Content-Type, Accept
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
