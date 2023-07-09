f52da5fcf05031bfb05c9be642f5c80aa08bc32a
Rochi Albani <rocio.albani@hotmail.com>
2023-07-08 23:18:59 -0300

    fixing cors problem

--- api/src/app.js
+++ api/src/app.js
@@ -26,7 +26,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://pi-dogs-theta-beige.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  //res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Content-Type');//Origin, X-Requested-With, Content-Type, Accept
   next();
 });
